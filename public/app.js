377a6a83842699848455af51e87629b128e29038
João Amaral <am.joao13@gmail.com>
2025-04-06 20:58:57 -0300

    Criação do JSON

---
+++ public/app.js
@@ -0,0 +1,66 @@
+const dados = [{
+    "id": 1, 
+    "titulo": "Alien, o 8.º Passageiro",
+    "descricao": "Uma nave espacial, ao retornar para Terra, recebe estranhos sinais vindos de um asteróide. Ao investigarem o local, um dos tripulantes é atacado por um estranho ser. O que parecia ser um ataque isolado se transforma em um terror constante, pois o tripulante atacado levou para dentro da nave o embrião de um alienígena, que não para de crescer e tem como meta matar toda a tripulação.",
+    "genero": "terror",
+    "diretor": "Ridley Scott",
+    "imagem": "/img/filme-alien.jpg"
+},
+{
+    "id": 2, 
+    "titulo": "Trem Bala",
+    "descricao": "Em Trem-Bala, Ladybug (Brad Pitt) é um assassino azarado, determinado a fazer seu trabalho pacificamente depois de muitas missões saírem dos trilhos. Quase desistindo de sua carreira, ele é recrutado por Maria Beetle (Sandra Bullock) para coletar uma maleta em um trem-bala indo de Tóquio para Morioka.",
+    "genero": "Ação / Comédia",
+    "diretor": "David Leitch",
+    "imagem": "/img/filme-trem-bala.jpeg"
+},
+{
+    "id": 3, 
+    "titulo": "Nobody",
+    "descricao": "Bob Odenkirk estrela como Hutch Mansell, um pai e marido subestimado e esquecido, que aguenta as injustiças da vida e nunca recua. Um anônimo. Quando dois ladrões invadem sua casa uma noite, Hutch se recusa a defender a si mesmo ou sua família, na esperança de evitar violência grave. Seu filho adolescente, Blake, está desapontado com ele e sua esposa, Becca, parece se afastar ainda mais.",
+    "genero": "Ação",
+    "diretor": "Ilya Naishuller",
+    "imagem": "/img/filme-nobody.jpg"
+},
+{
+    "id": 4, 
+    "titulo": "John Wick",
+    "descricao": "John Wick (Keanu Reeves) já foi um dos assassinos mais temidos da cidade de Nova York, trabalhando em parceria com a máfia russa. Um dia, ele decide se aposentar, e neste período tem que lidar com a triste morte de sua esposa. Vítima de uma doença grave, ela já previa a sua própria morte, e deu de presente ao marido um cachorro para cuidar em seu período de luto. No entanto, poucos dias após o funeral, o cachorro é morto por ladrões que roubam o seu carro. John Wick parte em busca de vingança contra estes homens que ele já conhecia muito bem, e que roubaram o último símbolo da mulher que ele amava.",
+    "genero": "Ação",
+    "diretor": " David Leitch, Chad Stahelski",
+    "imagem": "/img/filme-john-wick.jpeg"
+},
+{
+    "id": 5, 
+    "titulo": "Invincible",
+    "descricao": "Quando Mark Grayson herda superpoderes ao completar 17 anos, ele se une a seu pai como um dos maiores heróis da Terra. Todos os seus sonhos se tornam realidade - até que um evento chocante muda tudo.",
+    "genero": "Animação / Comédia",
+    "diretor": "Jeff Allen, Robert Valley, Haeyoung Jung, Paul Furminger, Youngsoo Kim, Hanhyung Lee, Cory Evans, Osamu Tsuruyama, Jay Baker, Jae Harm, Vinton Heuck, Minsoo Kim, William Ruzicka",
+    "imagem": "/img/serie-invincible.jpeg"
+},
+{
+    "id": 6, 
+    "titulo": "Fallout",
+    "descricao": "218 anos após o apocalipse, uma habitante pacífica de um agradável refúgio é forçada a se aventurar na superfície e fica chocada quando descobre a terra devastada que a espera.",
+    "genero": "",
+    "diretor": "Jonathan Nolan, Clare Kilner, Frederick E.O. Toye, Wayne Che Yip",
+    "imagem": "/img/serie-fallout.jpg"
+},
+{
+    "id": 7, 
+    "titulo": "The Batman",
+    "descricao": "Após dois anos espreitando as ruas como Batman, Bruce Wayne se encontra nas profundezas mais sombrias de Gotham City. Com poucos aliados confiáveis, o vigilante solitário se estabelece como a personificação da vingança para a população.",
+    "genero": "Ação, Suspense",
+    "diretor": "Matt Reeves",
+    "imagem": "/img/filme-batman.jpeg"
+},
+{
+    "id": 8, 
+    "titulo": "Entre Montanhas",
+    "descricao": "Dois agentes altamente treinados se aproximam à distância após serem enviados para proteger lados opostos de um desfiladeiro misterioso. Quando um mal emerge, eles precisam trabalhar juntos para sobreviver ao que está lá dentro.",
+    "genero": "Ação, Suspense",
+    "diretor": "Scott Derrickson",
+    "imagem": "/img/filme-entre-montanhas.jpg"
+},
+
+]
